f7b82db5a8d4ca7470b98c5fd18e967c9f3da862
cryinlaugh <cryinlaugh@gmail.com>
2020-02-04 10:43:39 +0800

    Version 1.0
    Add two files for getting login cookies for weibo.com and m.weibo.cn

---
+++ get_weibo_cookie_selenium.py
@@ -0,0 +1,53 @@
+#by zwl
+#thanks to https://blog.csdn.net/qq_42348937/article/details/85065104
+
+import pickle
+from selenium import webdriver
+
+def InitDriver(manually=1):
+	chrome_options = webdriver.ChromeOptions()
+	#no headless for manually login
+	#chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('window-size=1200,1100')
+	driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='./chromedriver-mac')
+	return driver
+
+def CloseDriver(driver):
+	driver.quit()
+
+def ManuallyLogin(url, driver):
+	driver.get(url)
+	ret = 0
+	while(ret != 'ok' ):
+		ret = input("请在网页中登陆微博账号，(登陆后输入ok继续):")
+	#print("current url is %s" % driver.current_url)
+	cookies = driver.get_cookies()
+	saveCookies = {}
+	for cookie in cookies:
+		saveCookies[cookie['name']] = cookie['value']
+	return saveCookies
+
+def SaveCookieToFile(cookies, filename):
+	filePath = open(filename, 'wb')
+	pickle.dump(cookies, filePath)
+	filePath.close()
+
+def SaveCookie():
+	# driver = InitDriver()
+	# cookies = ManuallyLogin("https://m.weibo.cn", driver)
+	# filename = "./WeiboCNCookies"
+	# SaveCookieToFile(cookies, filename)
+	# CloseDriver(driver)
+
+	driver = InitDriver()
+	cookies = ManuallyLogin("https://weibo.com", driver)
+	filename = "./WeiboCOMCookies"
+	SaveCookieToFile(cookies, filename)
+	CloseDriver(driver)
+
+def main():
+	SaveCookie()
+
+if __name__ == '__main__':
+	main()
